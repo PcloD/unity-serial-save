cad98f571c4f0dfe75417d27d05c5b9be9803324
Andrew Lord <andrewlord1990@gmail.com>
2016-12-08 08:34:31 +0000

    Fixed indentation on the counter button file.

--- src/SerialSave/Assets/Sample/CounterButton.cs
+++ src/SerialSave/Assets/Sample/CounterButton.cs
@@ -1,22 +1,22 @@
 ﻿namespace AndrewLord.UnitySerialSaveSample {
 
-	using AndrewLord.UnitySerialSave;
-	using UnityEngine;
+  using AndrewLord.UnitySerialSave;
+  using UnityEngine;
 
-	public class CounterButton : MonoBehaviour {
+  public class CounterButton : MonoBehaviour {
 
-		public SaveDataStore saveDataStore;
+    public SaveDataStore saveDataStore;
 
-		private SerialSaveStore saveStore;
+    private SerialSaveStore saveStore;
 
-		void Start() {
-			saveStore = saveDataStore.SaveStore;
-		}
+    void Start() {
+      saveStore = saveDataStore.SaveStore;
+    }
 
-		public void IncrementCounter() {
-			int currentCount = saveStore.GetValue(SaveDataStore.keyCounter);
-			saveStore.SetValue(SaveDataStore.keyCounter, currentCount + 1);
-			saveStore.Save();
-		}
-	}
+    public void IncrementCounter() {
+      int currentCount = saveStore.GetValue(SaveDataStore.keyCounter);
+      saveStore.SetValue(SaveDataStore.keyCounter, currentCount + 1);
+      saveStore.Save();
+    }
+  }
 }
